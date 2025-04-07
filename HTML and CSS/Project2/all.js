f2d72f66817c6b070e012d080b256ade5b888fa0
WilliamHsieh615 <best5946@gmail.com>
2025-04-08 00:13:01 +0800

    renew

--- HTML and CSS/Project2/all.js
+++ HTML and CSS/Project2/all.js
@@ -50,10 +50,26 @@ $(document).ready(function () {
         $('.ad').toggleClass('animate__animated animate__hinge');
     });
 
-    $('.adTitle').click(function (e) { 
+    // $('.adTitle').click(function (e) { 
+    //     e.preventDefault();
+    //     $('.message').addClass('show');
+    // });
+
+    $('.adTitle').click(function (e) {
         e.preventDefault();
+        $(".message .one, .message .two, .message .three, .message .fa-xmark").hide();
         $('.message').addClass('show');
-        
+        $(".message .one")
+        .text("Sorry...您錯過優惠了...")
+        .delay(2000).slideDown();
+        $(".message .two")
+        .text("下次請早")
+        .delay(3000).slideDown();
+        $(".message .three")
+        .text("請原價購買")
+        .delay(4000).slideDown();
+        $(".fa-solid.fa-xmark")
+        .delay(5000).slideDown();
     });
 
     $('.message .fa-xmark').click(function (e) { 
