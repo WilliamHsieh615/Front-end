bdde24070535f486acfc1efbc2c72bd5da233f5e
William Hsieh <best5946@gmail.com>
2025-05-29 02:16:46 +0800

    renew

--- Project12/JS/all.js
+++ Project12/JS/all.js
@@ -9,3 +9,31 @@ $(document).ready(function () {
 });
 
 
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    loop: true,
+    navigation: {
+        prevEl: ".leftArrow_JS",
+        nextEl: ".rightArrow_JS",
+    },
+
+    autoplay: {
+        delay: 3000, 
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true 
+    },
+
+    mousewheel: true,
+
+    breakpoints: {
+        800: {
+            slidesPerView: 2,
+        },
+        1200: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+
