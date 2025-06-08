c887be3745126ad6cbab4873eeea4e6e9df1f17f
William Hsieh <best5946@gmail.com>
2025-06-09 00:20:38 +0800

    renew

--- Project15/JS/cart.js
+++ Project15/JS/cart.js
@@ -1,24 +1,23 @@
-const items = document.querySelectorAll('.cart_main .item');
-
-items.forEach(item => {
+document.querySelectorAll('.item').forEach(item => {
     let startX = 0;
-    let endX = 0;
 
     item.addEventListener('touchstart', e => {
         startX = e.touches[0].clientX;
     });
 
     item.addEventListener('touchend', e => {
-        endX = e.changedTouches[0].clientX;
-        const distance = startX - endX;
+        const endX = e.changedTouches[0].clientX;
+        const diffX = startX - endX;
 
-        if (distance > 50) {
-            // 向左滑動
-            items.forEach(i => i.classList.remove('swiped')); // 關掉其他的
-            item.classList.add('swiped');
-        } else if (distance < -50) {
-            // 向右滑回去
-            item.classList.remove('swiped');
+        // 左滑：顯示該項目的 trashIcon
+        if (diffX > 50) {
+            document.querySelectorAll('.item').forEach(el => el.classList.remove('show-trash'));
+            item.classList.add('show-trash');
+        }
+
+        // 右滑：隱藏該項目的 trashIcon
+        if (diffX < -30) {
+            item.classList.remove('show-trash');
         }
     });
-});
+}); 
