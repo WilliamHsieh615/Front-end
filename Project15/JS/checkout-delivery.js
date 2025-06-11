5023fd465e044a1a32ecf26d42ddacd95e458c4b
William Hsieh <best5946@gmail.com>
2025-06-11 16:35:07 +0800

    renew

--- Project15/JS/checkout-delivery.js
+++
@@ -1,9 +0,0 @@
-$(document).on('click', '.readList', function (e) {
-    e.preventDefault();
-    $('.transactionDetail').toggleClass('showList');
-    if ($(this).siblings().hasClass('showList')) {
-        $(this).removeClass("bi-chevron-down").addClass("bi-chevron-up");
-    } else {
-        $(this).removeClass("bi-chevron-up").addClass("bi-chevron-down");
-    }
-});
